d1452fe248c3ff7848549b278d15a545a41f879b
trouve_b <alexis.trouve@epitech.eu>
2017-07-06 22:11:00 +0200

    depot struct

---
+++ Assets/Srcs/MenuScript/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ici on utilise un bibliothèque (library) pour les éléments d'interface
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public int test;
+    public int test2 = 5;
+    private int test3;
+    public GameObject buttontest;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
