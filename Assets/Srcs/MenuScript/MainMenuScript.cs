2eea57582dd4748b50a525a511f607c8764e83b0
Shuriloo <loizelet.julien@gmail.com>
2017-07-07 20:31:02 +0200

    add a button

--- Assets/Srcs/MenuScript/MainMenuScript.cs
+++ Assets/Srcs/MenuScript/MainMenuScript.cs
@@ -2,23 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// ici on utilise un bibliothèque (library) pour les éléments d'interface
-using UnityEngine.UI;
+// ici on utilise une bibliothèque (library) pour les éléments d'interface
+using UnityEngine.UI; 
 
-public class MainMenuScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour
+{
 
     public int test;
     public int test2 = 5;
     private int test3;
     public GameObject buttontest;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("start");
+       // Use this for initialization
+    void Start()
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+      
+    }
+
+    int Blabla (GameObject i)
+    {
+        return 1;
+    }
+
+    public void click(Button b)
+    {
+        Debug.Log("test");
+            
+    }
 }
